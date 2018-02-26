9b06b4a0b161232eea4e11e2c32f78db932be92b
ermasalmy <sweet88ema@yahoo.com>
2018-02-26 16:54:33 +0800

    add number

---
+++ AddNumber/src/AddNumber.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class AddNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Erma");
+	}
+
+}
---
+++ AddNumber/src/SecondNum.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class SecondNum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 9;
+		int y = 6;
+		int z = 5;
+		int sum;
+		System.out.println("Number 1= " + x);
+		System.out.println("Number 2= " + y);
+		System.out.println("Number 3= " + z);
+		
+		sum = x + y + z;
+		
+		System.out.println("The sum of three number is : " + sum);
+	}
+
+}
